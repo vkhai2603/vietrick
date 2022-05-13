8599ad2a21daa1fafd0f9b874a4ab7ebc828e9b9
KiMiDev <74590570+vkhai2603@users.noreply.github.com>
2022-05-13 15:50:56 +0700

    Update BlockIP.js

--- BlockIP.js
+++ BlockIP.js
@@ -2,13 +2,13 @@ function preloadFunc()
 {
 var ip = userip;
 var bannedips=[
- "162.158.179.239",
+ "162.158.179.239","202.92.7.198",
 ]
 var handleips=bannedips.join("|")
 handleips=new RegExp(handleips, "i")
 
 if (ip.search(handleips)!=-1){ 
-window.location.replace("https://www.BlockIp.Com.VN");
+window.location.replace("https://gotrangtri.vn/");
 document.write('<!---->');
 }
 }
